b052ca7d0490c4fa6d78fdf8d8d2749affb6d5c8
tom_glb <goodMorning_glb@atguigu.com>
2020-01-03 11:19:09 +0800

    edit on branch hot-fix

--- src/main/java/com/bj/wg/Happy.java
+++ src/main/java/com/bj/wg/Happy.java
@@ -4,5 +4,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Happy...Test...Right");
 		System.out.println("Happy...Test...Left");
+		
+		System.out.println("hot-fix...Left");
 	}
 }
